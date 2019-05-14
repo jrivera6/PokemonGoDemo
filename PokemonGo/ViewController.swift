bfec2c99f00315edce5fd48fee39b6f25d9ae658
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-05-14 16:11:54 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Jhonny Rivera on 5/14/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
